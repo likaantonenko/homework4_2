10d2dc09a9c03ddfa26132b1da9543dad13672dc
Angelika Antonenko <106235173+likaantonenko@users.noreply.github.com>
2023-09-15 20:24:36 +0300

    Upgrade task02

--- src/lesson3_task02.erl
+++ src/lesson3_task02.erl
@@ -1,14 +1,5 @@
 -module(lesson3_task02).
--export([words/1, words/2, first_word/1]).
-
-words(BinText) ->
-    words(BinText, <<>>).
-
-words(<<>>, Acc) ->
-    Acc;
-words(BinText, Acc) ->
-    {Slovo, Rest} = first_word(BinText),
-words(Rest, <<Acc/binary, Slovo>>).
+-export([words/1, first_word/1, rest_words/1]).
 
 first_word(BinText) -> 
     first_word(BinText, <<>>).
@@ -19,3 +10,23 @@ first_word(<<32, _/binary>>, Acc) ->
     Acc;
 first_word(<<Sym/utf8, Rest/binary>>, Acc) ->
     first_word(Rest, <<Acc/binary, Sym>>).
+
+rest_words(BinText) -> 
+    rest_words(BinText, <<>>).
+
+rest_words(<<>>) -> 
+    <<>>;
+rest_words(<<32/utf8, Rest/binary>>) ->  
+    Rest;
+rest_words(<<_/utf8, Rest/binary>>)->
+    rest_words(Rest).   
+
+words(BinText) ->
+    words(BinText, <<>>).
+words(<<>>, Acc) ->
+    Acc;
+words(BinText, Acc) ->
+    Slovo = first_word(BinText),
+    Rest = rest_words(BinText),
+    words(reverse(Rest, [Slovo | Acc])).    
+
